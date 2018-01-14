38b0470b67f5317c7775e089577f92dede2cd913
yuan <tianyuan.sun@daocloud.io>
2018-01-14 15:38:24 +0800

    feat: add exam view for client

--- api/haha_respond/haha_api/views.py
+++ api/haha_respond/haha_api/views.py
@@ -2,9 +2,12 @@
 from __future__ import unicode_literals
 import json
 from rest_framework import serializers, viewsets
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 from django.shortcuts import render, HttpResponse
 from .models import User
+from .models import Exam
 
 
 # Create your views here.
@@ -12,3 +15,15 @@ from .models import User
 
 def ping(*args, **kwargs):
     return HttpResponse("pong")
+
+class ExamSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Exam
+        fields = ('exam_id', 'name')
+
+
+class ExamApi(APIView):
+    def get(self, request, format=None):
+        exams = Exam.objects.all()
+        serializer = ExamSerializer(exams, many=True)
+        return Response(serializer)
