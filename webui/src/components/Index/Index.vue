9618cc65c7b93a12d2d7ab650ed3288259234238
Nighting.Cong <nighting@NightingCongdeMacBook-Pro.local>
2018-01-14 21:10:10 +0800

    feat(fe): 注册页

---
+++ webui/src/components/Index/Index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <van-loading type="gradient-circle" color="black" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+  data() {
+    return {
+    };
+  },
+  mounted() {
+    const user = localStorage.getItem('user');
+    if (user) {
+      // go to main page
+    } else {
+      this.$router.push({ name: 'Register' });
+    }
+  },
+};
+</script>
+
+<style lang="scss">
+
+</style>
--- webui/src/router/index.js
+++ webui/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import HelloWorld from '@/components/HelloWorld';
+import Index from '@/components/Index/Index';
+import Reg from '@/components/Reg/Reg';
 
 Vue.use(Router);
 
@@ -8,8 +9,13 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld,
+      name: 'Index',
+      component: Index,
+    },
+    {
+      path: '/register',
+      name: 'Register',
+      component: Reg,
     },
   ],
 });
