4266bf8c70d9bcbcaa1cf9f6cd6e9626a177548c
Nighting.Cong <nighting@NightingCongdeMacBook-Pro.local>
2018-01-15 15:33:43 +0800

    feat(fe): 增加 logo

--- webui/src/components/Reg/Reg.vue
+++ webui/src/components/Reg/Reg.vue
@@ -20,9 +20,17 @@ export default {
       const name = this.username;
       const email = this.email;
 
+      const emailRegex = new RegExp(/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/); // eslint-disable-line
+
       if (!name || !email) {
         this.showDialog('昵称及邮箱均不能为空');
+        return false;
+      } else if (!emailRegex.test(email)) {
+        this.showDialog('邮箱格式错误');
+        return false;
       }
+
+      return true;
     },
     showDialog(text) {
       Dialog.alert({
@@ -32,7 +40,10 @@ export default {
       });
     },
     submit() {
-      this.checkValid();
+      const valid = this.checkValid();
+      if (valid) {
+        // reg api
+      }
     },
   },
 };
