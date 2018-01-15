fff862a1180b036ee4d367c9db7a70af060cf5b6
Nighting.Cong <nighting@NightingCongdeMacBook-Pro.local>
2018-01-15 15:56:54 +0800

    feat(fe): 增加对 daocloud.io 邮箱的验证

--- webui/src/components/Reg/Reg.vue
+++ webui/src/components/Reg/Reg.vue
@@ -9,10 +9,6 @@ export default {
     return {
       username: '',
       email: '',
-      valid: {
-        show: false,
-        text: '',
-      },
     };
   },
   methods: {
@@ -21,6 +17,7 @@ export default {
       const email = this.email;
 
       const emailRegex = new RegExp(/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/); // eslint-disable-line
+      const daoEmailRegex = new RegExp(/@daocloud\.com/);
 
       if (!name || !email) {
         this.showDialog('昵称及邮箱均不能为空');
@@ -28,6 +25,8 @@ export default {
       } else if (!emailRegex.test(email)) {
         this.showDialog('邮箱格式错误');
         return false;
+      } else if (!daoEmailRegex.test(email)) {
+        this.showDialog('必须为 daocloud.io 域名的邮箱');
       }
 
       return true;
