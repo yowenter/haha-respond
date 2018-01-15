23dcc988f9d87ebafa88101835daac0a28a2feb1
Taoge <wenter.wu@gmail.com>
2018-01-15 17:50:54 +0800

    > nginx static file

--- api/haha_respond/haha_respond/settings.py
+++ api/haha_respond/haha_respond/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATIC_ROOT = os.path.join(os.path.dirname(os.path.dirname(__file__)), '_static')
+#
+# STATIC_ROOT = os.path.join(os.path.dirname(os.path.dirname(__file__)), '_static')
 
 
