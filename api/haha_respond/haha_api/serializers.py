55ced11e9a77e839e383b840866e9a4b49948598
Taoge <wenter.wu@gmail.com>
2018-01-15 16:52:43 +0800

    > username email check

--- api/haha_respond/haha_api/serializers.py
+++ api/haha_respond/haha_api/serializers.py
@@ -9,6 +9,7 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
 
     username = serializers.CharField(max_length=64, allow_blank=False, min_length=2)
     email = serializers.CharField(max_length=64, allow_blank=False, min_length=10)
+
     # password = serializers.CharField(max_length=64, allow_blank=False, min_length=6)
 
     def create(self, validated_data):
@@ -22,6 +23,12 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
         super(UserSerializer, self).is_valid()
         if not str(self.validated_data.get('email', '')).endswith('daocloud.io'):
             raise exceptions.ValidationError(detail="Email should endswith daocloud.io ", code=400)
+        email = self.validated_data['email']
+        if User.objects.filter(email=email).first():
+            raise exceptions.ValidationError(detail="Email %s exists" % email, code=409)
+
+        if User.objects.filter(username=self.validated_data['username']).first():
+            raise exceptions.ValidationError(detail="Name %s exists" % email, code=409)
 
         return True
 
