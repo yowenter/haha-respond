73b39fa317573800520670d01b80dd8ea7ef33bd
MimoN <mimo.ouyang@daocloud.io>
2018-01-16 16:53:21 +0800

    feat(rank-top): 完成排行榜前三

--- webui/src/components/Rank/Rank.vue
+++ webui/src/components/Rank/Rank.vue
@@ -1,6 +1,9 @@
-<template src="./Rank.html"></template>
+<template src="./Rank.html">
+</template>
 
 <script>
+import { clone } from 'lodash';
+
 import TextAvatar from '../text-avatar';
 
 export default {
@@ -19,6 +22,11 @@ export default {
         '#53C0CA',
         '#85C486',
       ],
+      topColors: [
+        'rgb(170,170,170)',
+        'rgb(245,193,105)',
+        'rgba(194,9,9,0.5)',
+      ],
       randomColorNumber: 7,
       ranks: [{
         username: 'Test1',
@@ -27,21 +35,62 @@ export default {
       }, {
         username: 'Cin2',
         email: 'aa@daocloud.io',
-        score: 111,
+        score: 112,
       }, {
         username: 'Mimo3',
         email: 'aa@daocloud.io',
-        score: 111,
+        score: 113,
       }, {
         username: 'Nighting4',
         email: 'aa@daocloud.io',
-        score: 111,
+        score: 114,
       }, {
         username: 'Cisy5',
+        email: 'aa@daocloud.io',
+        score: 115,
+      }, {
+        username: 'Test',
+        email: 'aa@daocloud.io',
+        score: 111,
+      }, {
+        username: 'Test2',
+        email: 'aa@daocloud.io',
+        score: 111,
+      }, {
+        username: 'Test3',
+        email: 'aa@daocloud.io',
+        score: 111,
+      }, {
+        username: 'Test4',
+        email: 'aa@daocloud.io',
+        score: 111,
+      }, {
+        username: 'Test5',
         email: 'aa@daocloud.io',
         score: 111,
       }],
     };
+  },
+  computed: {
+    sortedRanks() {
+      return clone(this.ranks).sort((a, b) => b.score - a.score);
+    },
+    topRanks() {
+      const topRanks = this.sortedRanks.slice(0, 3);
+      return [{
+        index: 2,
+        ...topRanks[1],
+      }, {
+        index: 1,
+        ...topRanks[0],
+      }, {
+        index: 3,
+        ...topRanks[2],
+      }]
+    },
+    otherRanks() {
+      return this.sortedRanks.slice(3);
+    },
   },
   mounted() {
     // const user = localStorage.getItem('user');
@@ -50,11 +99,12 @@ export default {
     // }
   },
   methods: {
-    getIcon(username, index) {
+    getIcon(username, index, otherData = {}) {
       return {
         text: username.slice(0, 1),
         color: '#ffffff',
         bg: this.colors[index % this.randomColorNumber],
+        ...otherData,
       };
     },
   },
