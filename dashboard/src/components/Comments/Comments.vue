d4baa09608129914b9ab70a3a22ca674f2e23432
Nighting.Cong <nighting@NightingCongdeMacBook-Pro.local>
2018-01-16 18:53:48 +0800

    feat(dash): comment

---
+++ dashboard/src/components/Comments/Comments.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="comments">
+    <comment v-for="(co, index) in comments"
+      :key="co.time"
+      :data="co"
+      :index="index"
+      @end="destory"
+    ></comment>
+  </div>
+</template>
+
+<script>
+import { bus } from '@/model/const';
+import Comment from '@/components/Comment/Comment';
+
+export default {
+  name: 'Comments',
+  components: {
+    Comment,
+  },
+  data() {
+    return {
+      comments: [],
+    };
+  },
+  mounted() {
+    bus.$on('comment', (comment) => {
+      this.comments.push(comment);
+    });
+  },
+  methods: {
+    destory(index) {
+      this.comments.splice(index, 1);
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.comments {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 99;
+  overflow: hidden;
+}
+</style>
